adcadf0602d7f833db76e6c67c0b1fc3b04a023f
Marcel Candido <phpmarcel@gmail.com>
2018-05-03 16:27:25 -0300

    Commit Inicial

---
+++ src/DOMPDFModule/Mvc/Service/ViewPdfRendererFactory.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Raymond J. Kolbe <raymond.kolbe@maine.edu>
+ * @copyright Copyright (c) 2012 University of Maine
+ * @license	http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace DOMPDFModule\Mvc\Service;
+
+use DOMPDFModule\View\Renderer\PdfRenderer;
+use Interop\Container\ContainerInterface;
+use Interop\Container\Exception\ContainerException;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+class ViewPdfRendererFactory implements FactoryInterface
+{
+
+    /**
+     * Create an object
+     *
+     * @param  ContainerInterface $container
+     * @param  string $requestedName
+     * @param  null|array $options
+     * @return object
+     * @throws ServiceNotFoundException if unable to resolve the service.
+     * @throws ServiceNotCreatedException if an exception is raised when
+     *     creating a service.
+     * @throws ContainerException if any other error occurs
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $viewManager = $container->get('ViewManager');
+
+        $pdfRenderer = new PdfRenderer();
+        $pdfRenderer->setResolver($container->get('ViewResolver'));
+        $pdfRenderer->setHtmlRenderer($container->get('ViewRenderer'));
+        $pdfRenderer->setEngine($container->get('dompdf'));
+
+        return $pdfRenderer;
+    }
+}
